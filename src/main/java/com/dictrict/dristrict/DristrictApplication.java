a57a605ffb4ade4545fb3d5f8f73916c60b3f9e4
tiwarisanjeevkumar <sanjeevtiwari1980@gmail.com>
2025-04-01 12:46:53 +0530

    commited

---
+++ src/main/java/com/dictrict/dristrict/DristrictApplication.java
@@ -0,0 +1,13 @@
+package com.dictrict.dristrict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DristrictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DristrictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dictrict/dristrict/DristrictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dictrict.dristrict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DristrictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
